4c3dba7e4c31292e38fe1431263682d403f2959e
Simone Smith <simone.smith@guardian.co.uk>
2022-08-01 13:49:04 +0100

    Initial Commit

---
+++ FrontsV2Prototype/FrontsV2PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontsV2PrototypeApp.swift
+//  FrontsV2Prototype
+//
+//  Created by Simone Smith on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FrontsV2PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
